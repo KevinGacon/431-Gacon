5db28f83e81ae730f49592ff40fc5e8f79b102d6
KevinGacon <48351155+KevinGacon@users.noreply.github.com>
2019-04-05 10:50:59 +0200

    Add files via upload

---
+++ Demarage.h
@@ -0,0 +1,39 @@
+//
+// Created by Kevin.GACON on 04.04.2019.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <locale.h>
+
+#ifndef BATAILLENAVALE_DEMARAGE_H
+#define BATAILLENAVALE_DEMARAGE_H
+
+#endif //BATAILLENAVALE_DEMARAGE_H
+
+int Demarage () {
+    system("cls");
+    printf("                    ()\n"
+           "                    ||q',,'\n"
+           "                    ||d,~\n"
+           "         (,---------------------,)\n"
+           "          ',       q888p       ,'\n"
+           "            \\       986       /\n"
+           "             \\  8p, d8b ,q8  /\n"
+           "              ) 888a888a888 (\n"
+           "             /  8b` q8p `d8  \\              O\n"
+           "            /       689       \\             |','\n"
+           "           /       d888b       \\      (,---------,)\n"
+           "         ,'_____________________',     \\   ,8,   /\n"
+           "         (`__________L|_________`)      ) a888a (    _,_\n"
+           "         [___________|___________]     /___`8`___\\   }*{\n"
+           "           }:::|:::::}::|::::::{      (,=========,)  -=-\n"
+           "            '|::::}::|:::::{:|'  .,.    \\:::|:::/    ~`~=\n  "
+           "             '|}:::::|::{:::|'          ~\".,.\"~`~\n"
+           "               '|:}::|::::|'~`~\".,.\"\n"
+           "           ~`~\".,.\"~`~\".,                 \"~`~\".,.\"~\n"
+           "                          \".,.\"~`~\n");
+    system("pause");
+}
